cc9c30e8ef989138d812054b9aeaa9a885526765
Albert Portnoy <albert@albertp.dev>
2023-02-09 20:12:37 -0600

    Channel header

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
-import { common } from "replugged";
+import { Injector, Logger, common, webpack } from "replugged";
 const { lodash: _, parser } = common;
+const { filters, getFunctionKeyBySource, waitForModule } = webpack;
+import type React from "react";
+
+const logger = Logger.plugin("dev.albertp.TwemojiEverywhere");
+const injector = new Injector();
 
 let emojiParser: ReturnType<typeof parser.reactParserFor>;
+
+type ChannelHeader = React.FC & {
+  Title: (...args: unknown[]) => unknown;
+};
+
+async function patchChannelHeader(): Promise<void> {
+  const headerMod = await waitForModule<Record<string, ChannelHeader>>(
+    filters.bySource(/\w+.Icon=\w+;\w+\.Title=/),
+  );
+  const headerModKey = getFunctionKeyBySource("().toolbar", headerMod);
+  if (!headerModKey) {
+    logger.warn("Could not find channel header module");
+    return;
+  }
+  injector.before(headerMod, headerModKey, ([args]) => {
+    const childArr = _.get(args, "children[0].props.children[1].props.children.props.children");
+    if (!childArr) return;
+    if (!Array.isArray(childArr)) return;
+    if (typeof childArr[2] !== "string") return;
+    childArr[2] = patchText(childArr[2]);
+  });
+}
 
 export function start(): void {
   const rules = _.pick(parser.defaultRules, ["text", "emoji"]);
   emojiParser = parser.reactParserFor(rules);
+
+  void patchChannelHeader();
 }
 
-export function patchText(text: string): void {
+export function stop(): void {
+  injector.uninjectAll();
+}
+
+export function patchText(text: string): React.ReactElement {
   return emojiParser(text);
 }
